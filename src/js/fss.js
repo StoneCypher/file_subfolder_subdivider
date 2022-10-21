922e6649bf35a88af6b8ce5fb6724103e4985a78
John Haugeland <stonecypher@gmail.com>
2022-10-21 10:46:32 -0700

    range under control

--- src/js/fss.js
+++ src/js/fss.js
@@ -29,7 +29,7 @@
 
 
 
-// var { spawn, exec } = require('child_process');
+const { execSync } = require('child_process');
 
 
 
@@ -37,7 +37,7 @@
 
 const default_range_digits = 3,
       default_mask_digits  = 4,
-      default_mask         = 'frame$$.png',
+      default_mask         = 'frame##.png',
       default_parallelism  = 1;
 
 
@@ -47,10 +47,12 @@ const default_range_digits = 3,
 const { program } = require('commander');
 
 program
-  .option('-d, --mask-digits <number>',  'count of question marks in the file mask', default_mask_digits.toString())
-  .option('-r, --range-digits <number>', 'digits to expand to; use 0 to not expand', default_range_digits.toString())
-  .option('-m, --mask <string>',         'text mask for filename; use $$ for inset', default_mask)
-  .option('-p, --parallelism <number>',  'maximum movers to run in parallel',        default_parallelism);
+  .option('-t, --test',                  'explain the arguments given')
+  .option('-q, --quiet',                 'do not log progress')
+  .option('-d, --mask_digits <number>',  'count of question marks in the file mask', default_mask_digits.toString())
+  .option('-r, --range_digits <number>', 'digits to expand to; use 0 to not expand', default_range_digits.toString())
+//.option('-p, --parallelism <number>',  'maximum movers to run in parallel',        default_parallelism.toString())
+  .option('-m, --mask <string>',         'text mask for filename; use $$ for inset', default_mask);
 
 program.parse();
 
@@ -59,25 +61,38 @@ program.parse();
 
 
 const options      = program.opts(),
+      is_quiet     = options['quiet']        ?? false,
       parallelism  = options['parallelism']  ?? default_parallelism,
-      range_digits = options['range-digits'] ?? default_range_digits,
-      mask_digits  = options['mask-digits']  ?? default_mask_digits,
+      range_digits = options['range_digits'] ?? default_range_digits,
+      mask_digits  = options['mask_digits']  ?? default_mask_digits,
       mask         = options['mask']         ?? default_mask;
 
+const [ mask_lo,
+        mask_hi ]  = mask.split('##');
 
 
 
 
-function filename_instance(index, range_digits, mask_digits, mask) {
 
-  const [ lo, hi ] = mask.split('$$');
-  if (lo === undefined) { throw 'mask completely missing'; }
-  if (hi === undefined) { throw 'mask missing $$ breakpoint'; }
+const seq = n =>
+  new Array(n).fill(0).map( (_, i) => i );
 
-  const inset   = index.toString().padStart(range_digits, '0'),
-        maskset = ''.padStart(mask_digits, '?');
+const dec_range = n =>
+  seq( Math.pow(10, n) );
 
-  return `${lo}${inset}${maskset}${hi}`;
+
+
+
+
+function filename_instance(index, u_range_digits, u_mask_digits, mask) {
+
+  if (mask_lo === undefined) { throw 'mask completely missing'; }
+  if (mask_hi === undefined) { throw `mask missing ## breakpoint (mask_lo is ${mask_lo})`; }
+
+  const inset   = index.toString().padStart(u_range_digits, '0'),
+        maskset = ''.padStart(u_mask_digits, '?');
+
+  return `${mask_lo}${inset}${maskset}${mask_hi}`;
 
 }
 
@@ -85,8 +100,90 @@ function filename_instance(index, range_digits, mask_digits, mask) {
 
 
 
-function bootstrap() {
+const l_move_command = (u_index, u_range_digits, u_mask_digits, u_mask) =>
+  `mv ${filename_instance(u_index, u_range_digits, u_mask_digits, u_mask)} ./${index}/ `;
+
+const w_move_command = (index, range_digits, mask_digits, mask) =>
+  `move ${filename_instance(index, range_digits, mask_digits, mask)} .\${index} `;
+
+
+function move_command(index, range_digits, mask_digits, mask) {
+
+  const pad_index = index.toString().padStart(range_digits, '0');
+  if (!(is_quiet)) { console.log(`  Filling ${pad_index}`); }
+
+  // execSync(
+  //   is_win
+  //     ? w_move_command(index, range_digits, mask_digits, mask)
+  //     : l_move_command(index, range_digits, mask_digits, mask)
+  // );
+
+}
+
+
+
+
+
+const  l_mkdir = index =>
+  ``;
+
+const  w_mkdir = index =>
+  `mkdir ${index}`;
+
+
+function mkdir(u_index, u_range_digits) {
+
+  const pad_index = u_index.toString().padStart(u_range_digits, '0');
+  if (!(is_quiet)) { console.log(`\nMaking directory ${pad_index}`); }
+
+  // execSync(
+  //   is_win
+  //     ? w_mkdir(pad_index)
+  //     : l_mkdir(pad_index)
+  // );
+
+}
+
+
+
+
+
+function test() {
+
+  const trim = '9876543210'.substring(0, range_digits);
+
   console.log(`range ${range_digits}, mask ${mask_digits} ${mask}, parallelism ${parallelism}`);
+  console.log(`  example: ${filename_instance(0,    range_digits, mask_digits, 'example##.png')}`);
+  console.log(`  example: ${filename_instance(trim, range_digits, mask_digits, 'example##.png')}`);
+  console.log(`  example: ${move_command(     trim, range_digits, mask_digits, 'example##.png')}`);
+
+}
+
+
+
+
+
+function make_range(u_dir_idx) {
+  mkdir(u_dir_idx);
+  move_command(u_dir_idx, range_digits, mask_digits, mask)
+}
+
+
+
+
+
+function bootstrap() {
+
+  if (options['test']) {
+    test();
+  } else {
+
+    const dec = dec_range(range_digits);
+
+    dec.map(d => make_range(d));
+
+  }
+
 }
 
 bootstrap();
